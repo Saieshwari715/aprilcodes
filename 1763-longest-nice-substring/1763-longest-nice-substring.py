5b81a40d6574b767bd0ca520aa7ddb185ec38056
Saieshwari715 <gajulasaieshwari0131@gmail.com>
2026-06-10 21:48:25 +0530

    Time: 33 ms (40.1%), Space: 19.2 MB (95.42%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -4,10 +4,10 @@ class Solution:
         for i in range(len(s)):
             for j in range(i+1,len(s)+1):
                 sub=s[i:j]
-                st=set(sub)
+                
                 nice=True
-                for ch in st:
-                    if ch.swapcase() not in st:
+                for ch in sub:
+                    if ch.swapcase() not in sub:
                         nice=False
                         break
                 if nice and len(sub)>len(ans):
